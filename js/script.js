0f274d5c701e96bac42ad49dd97d201232b79bdb
Juan1922 <80922568+Juan1922@users.noreply.github.com>
2021-04-18 16:19:40 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 300,
+        arrows: true,
+        fade: true,
+        pauseOnHover: false
+    });
+});
